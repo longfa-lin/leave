55fabf1bea3d65b3cd1045e505655edcaf502df6
linlongfa <linlongfa@seassoon.com>
2023-11-21 18:06:00 +0800

    新建认证模块

---
+++ auth-service/src/main/java/com/vian/auth/service/interfaces/facade/AuthApi.java
@@ -0,0 +1,12 @@
+package com.vian.auth.service.interfaces.facade;
+
+/**
+ * @Description: TODO
+ * @ClassName: AuthApi
+ * @Author: lin long fa
+ * @Date: 2023-11-21 18:05:04
+ * @Version: v0.0.1
+ * @Edit: Number Date User Remark
+ **/
+public class AuthApi {
+}
