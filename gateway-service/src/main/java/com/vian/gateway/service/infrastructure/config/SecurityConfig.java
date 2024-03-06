3b6934efc166313cc6addb4a637a7419d6fbaed9
linlongfa <linlongfa@seassoon.com>
2024-03-06 18:28:47 +0800

    test

--- gateway-service/src/main/java/com/vian/gateway/service/infrastructure/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/vian/gateway/service/infrastructure/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.vian.gateway.service.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -16,6 +17,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration(proxyBeanMethods = false)
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/webjars/**", "/assets/**");
+    }
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
